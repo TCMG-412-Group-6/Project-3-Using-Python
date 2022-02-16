d53dc1a889f57020cfdeeb98b420d88dbabd8489
Bcnovacane <98492218+Bcnovacane@users.noreply.github.com>
2022-02-16 13:56:52 -0600

    beginning code

---
+++ britany.py
@@ -0,0 +1,7 @@
+#Britany Cano
+from urllib.request import urlretrieve
+
+url_path="https://s3.amazonaws.com/tcmg476/http_access_log"
+log_file='log_copy.log'
+
+log_file, headers= urlretrieve(url_path, log_file, lamba x,y,z: print('.', end='',flush=True) if x % 100 == 0 else False)
