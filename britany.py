ee68b1ceae6641a826d796bee609590fb353a8b5
Bcnovacane <98492218+Bcnovacane@users.noreply.github.com>
2022-02-16 14:12:01 -0600

    changes2

--- britany.py
+++ britany.py
@@ -1,7 +1,11 @@
 #Britany Cano
+#url file request
 from urllib.request import urlretrieve
-
+#recevies url link
 url_path="https://s3.amazonaws.com/tcmg476/http_access_log"
 log_file='log_copy.log'
 
-log_file, headers= urlretrieve(url_path, log_file, lamba x,y,z: print('.', end='',flush=True) if x % 100 == 0 else False)
+log_file, headers= urlretrieve(url_path, log_file, lambda x,y,z: print('.', end='',flush=True) if x % 100 == 0 else False)
+
+f=open(log_file)
+lines= f.readlines()
